d6d670a0283b3ad330b67911198a224fff0b235a
Andrew Rogers <andrewprogers@gmail.com>
2019-12-05 17:29:15 -0500

    create input box

---
+++ src/components/InputBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const InputBox = ({id, value, onChange}) => {
+    let changeHandler = event => {
+        onChange(event.target.value)
+    }
+
+    return <div className="form-group shadow-textarea">
+        <label htmlFor="exampleFormControlTextarea6">Text to Encode:</label>
+        <textarea 
+            className="form-control z-depth-1" 
+            id={id}
+            rows="3" 
+            value={value}
+            onChange={changeHandler}
+            placeholder="Write something here..."></textarea>
+    </div>
+}
+
+export default InputBox;
