05955a8cf3e2216ddd7d1eab0f13cbad5d9e5d50
Andrew Rogers <andrewprogers@gmail.com>
2019-12-05 16:05:04 -0500

    add react smoke test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return(<h1>Smoke test!</h1>)
+    }
+}
+
 // must import web assembly code asynchronously
 import('./wasm_pkg/encode_demo_wasm')
 .then(wasm => {
-    wasm.greet("Bob");
+    //wasm.greet("Andrew");
+    let root = document.getElementById('react-root');
+    console.log(root);
+    ReactDOM.render(<App />, root);
 })
