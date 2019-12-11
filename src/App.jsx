39612ff207d30d4e3c2b1d9f481bbf1af137a769
Andrew Rogers <andrewprogers@gmail.com>
2019-12-11 15:03:05 -0500

    add byte visualizer

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import sampleText from './misc_assets/melville_ch1.txt';
 import {Container, Row, Col } from 'react-bootstrap';
 
 import InputBox from './components/InputBox.jsx';
+import ByteVisualizer from './components/ByteVisualizer/ByteVisualizer';
+
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.encoder = new TextEncoder('utf-8');
+
         this.state = {
             input: sampleText
         }
@@ -27,6 +31,13 @@ class App extends React.Component {
                     <Col></Col>
                     <Col xs={12} md={8}>
                         <InputBox value={this.state.input} onChange={this.updateInput}/>
+                        <ByteVisualizer
+                            bytes={this.encoder.encode(this.state.input)}
+                            bitWidth={5}
+                            bitHeight={20}
+                            borderWidth={1}
+                            maxHeight={200}
+                        />
                     </Col>
                     <Col></Col>
                 </Row>
@@ -36,3 +47,8 @@ class App extends React.Component {
 }
 
 export default App;
+
+// changed anonymous func to class function
+// .002 - .004 ms/byte
+// only calculate masks once per run
+// .001 - .0025 
